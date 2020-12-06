e42d07b51c9121df87a4dbea9fdf6565a0e90afd
DevinCastro <dcrocker21@gmail.com>
2020-12-05 22:51:47 -0800

    MVC using home controller to render home view

---
+++ HelloASP/HelloASP/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HelloASP.Models;
+
+namespace HelloASP.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet("about")]
+        public string About()
+        {
+            return "This is the about page";
+        }
+
+        [HttpGet("users/{username}")]
+        public string Username(string username)
+        {
+            return $"whats up {username}?";
+        }
+
+
+    }
+}
